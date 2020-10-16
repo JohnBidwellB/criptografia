c899372ca2b08b047eb490090647f56ef81b7c31
John Bidwell <johnbidwellb@gmail.com>
2020-10-16 03:21:07 -0300

    Fix in python scrypt

--- Tarea3/main.py
+++ Tarea3/main.py
@@ -11,8 +11,8 @@ from sys import exit
 
 BLOCK_SIZE = 16
 SEGMENT_SIZE = 128
-KEY = "2SQf1WNQnxYleSmg"  # Debe ser de 16 caracteres
-text_to_encrypt = "Text to encrypt"
+KEY = "2SQf1WNQnxYleSmg"  # Debe ser de BLOCK_SIZE caracteres
+text_to_encrypt = "Hola criptografía"
 
 
 def pad(data):
@@ -38,12 +38,16 @@ def decrypt(encrypted, passphrase):
 
 
 def generate_HTML_file(cipher_text):
-    html_text = """<p>Este sitio contiene un mensaje secreto</p>"""
+    html_text = """<p id="title">Este sitio contiene un mensaje secreto</p>"""
     html_div = """<div class="algorithm" id="%s"></div>""" % (cipher_text)
+    html_decodedtext = """<div id="decodedText"></div>"""
+    html_key = """<div class="algorithmKey" id="%s"></div>""" % KEY
 
     html_file = open('index.html', 'w')
     html_file.write(html_text)
     html_file.write(html_div)
+    html_file.write(html_key)
+    html_file.write(html_decodedtext)
 
 
 if __name__ == "__main__":
@@ -51,10 +55,10 @@ if __name__ == "__main__":
         description="Permite cifrar un mensaje utilizando el algoritmo Rijndael"
     )
 
-    parser.add_argument("-b", "--blocksize", dest="blocksize", type=int, default=16,
-                        help="Tamaño del bloque (16, 24 o 32)")
-    parser.add_argument("-s", "--segmentsize", dest="segmentsize", type=int, default=128,
-                        help="Tamaño del segmento (128, 196 o 256 bits). Debe coincidir en bytes con el tamaño del bloque")
+    # parser.add_argument("-b", "--blocksize", dest="blocksize", type=int, default=16,
+    #                     help="Tamaño del bloque (16, 24 o 32)")
+    # parser.add_argument("-s", "--segmentsize", dest="segmentsize", type=int, default=128,
+    #                     help="Tamaño del segmento (128, 192 o 256 bits). Debe coincidir en bytes con el tamaño del bloque")
     parser.add_argument("-k", "--key", dest="key", type=str, default="2SQf1WNQnxYleSmg",
                         help="Llave para cifrar/descifrar el mensaje, debe tener la misma cantidad de caracteres que el tamaño del bloque")
     parser.add_argument("-t", "--text", dest="text",
@@ -62,14 +66,16 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    if (args.blocksize == 16 and args.segmentsize == 128 and len(args.key) == 16 or args.blocksize == 24 and args.segmentsize == 196 and len(args.key) == 24 or args.blocksize == 32 and args.segmentsize == 256 and len(args.key) == 32):
-        BLOCK_SIZE = args.blocksize
-        SEGMENT_SIZE = args.segmentsize
+    # if (args.blocksize == 16 and args.segmentsize == 128 and len(args.key) == 16 or args.blocksize == 24 and args.segmentsize == 192 and len(args.key) == 24 or args.blocksize == 32 and args.segmentsize == 256 and len(args.key) == 32):
+    if (len(args.key) == 16):
+        # BLOCK_SIZE = args.blocksize
+        # SEGMENT_SIZE = args.segmentsize
+        KEY = args.key
         if args.text:
             text_to_encrypt = args.text
 
         cipher_text = encrypt(text_to_encrypt, KEY)
         generate_HTML_file(cipher_text)
     else:
-        print('Valores inválidos. Utilice el comand -h para obtener mayor información del scrypt.')
+        print('Valores inválidos. Utilice el comando -h para obtener mayor información del scrypt.')
         exit()
